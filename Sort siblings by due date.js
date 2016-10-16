55ca3afa9d5731f3db8ce7fbefff72237ddb8115
yrammos <yannis.rammos@me.com>
2016-10-17 00:38:40 +0300

    Add script to sort siblings by due date.

---
+++ Sort siblings by due date.js
@@ -0,0 +1,32 @@
+// SORT SELECTED LINES BY DUE DATE
+(function() {
+    function TaskPaperContextScript(editor, options) {
+        var selection = editor.selection.startItem, // Get the selected node (or the starting item of a selected range)...
+            selection_parent = selection.parent;    // ... and the parent of the selected node.
+
+        editor.outline.groupUndoAndChanges(
+            function() {
+                var sorted_siblings = editor
+                        .outline
+                        .evaluateItemPath('following-sibling::* union (descendant-or-self::* intersect ancestor-or-self::*) union preceding-sibling::*', selection)     // Obtain the item path across all siblings of the selected node...
+                        .sort(function (a, b) {                 // ... and sort said siblings by due date.
+                                var strA = a.hasAttribute('data-due') ? a.getAttribute('data-due', Date, false) : new Date (00, 0);
+                                    strB = b.hasAttribute('data-due') ? b.getAttribute('data-due', Date, false) : new Date (00, 0);
+                                return (strA !== strB) ? (strA < strB ? -1 : 1) : 0;
+                            })
+
+                sorted_siblings.forEach(    // Uproot the unsorted nodes from their common parent...
+                    function(x) {x.removeFromParent()}
+                );
+
+                selection_parent.appendChildren(sorted_siblings); //... and graft them back in the right order.
+            }
+        );
+    }
+
+    tp3 = Application('TaskPaper');
+    ds = tp3.documents;
+    return ds.length ? ds[0].evaluate({
+        script: TaskPaperContextScript.toString()
+    }) : undefined;
+})();
